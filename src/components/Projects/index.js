43886dbe9e62247677c13d8d1ea5ffacf61f4f66
itsmearun1 <arundevvb1@gmail.com>
2023-11-21 11:01:45 +0530

    new

---
+++ src/components/Projects/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+        I've been involved in diverse projects,showcasing my versatility and expertise across different platforms.Here are some of my projects.
+        </Desc>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal} />
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal} />
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
---
+++ src/themes/default.js
@@ -0,0 +1,26 @@
+export default {
+  // Updated fonts
+  fonts: {
+    title: "Roboto, sans-serif",
+    main: "Roboto, sans-serif"
+  },
+  // Refreshed colors for a modern look
+  colors: {
+    primary1: "#3498db",
+    background1: "#ecf0f1",
+    button: "#3498db",
+    background2: "#bdc3c7",
+    text: "#2c3e50",
+    text1: "#ffffff",
+    text2: "#34495e",
+    text3: "#2e4053",
+    footerBackground: "#2c3e50"
+  },
+  // Adjusted breakpoints for better responsiveness
+  breakpoints: {
+    sm: 'screen and (max-width: 576px)',
+    md: 'screen and (max-width: 768px)',
+    lg: 'screen and (max-width: 992px)',
+    xl: 'screen and (max-width: 1200px)'
+  },
+}
